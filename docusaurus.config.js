25385899afafeabf4c1c7a0b1ade2e958c706577
Ruqi <liruqi@gmail.com>
2026-09-15 05:08:19 +0800

    更新生成脚本

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,9 +84,9 @@ const config = {
   ],
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.16.47/dist/katex.min.css',
       type: 'text/css',
-      integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      integrity: 'sha384-nH0MfJ44wi1dd7w6jinlyBgljjS8EJAh2JBoRad8a3VDw2K69vfaaqm4WnR+gXtA',
       crossorigin: 'anonymous',
     },
   ],
